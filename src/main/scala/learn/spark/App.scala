8c31742351a1767ad3c8a210ca5c53d54cd54546
qiuzhanghua <qiuzhanghua@icloud.com>
2022-07-25 07:36:34 +0800

    spark sql count

--- src/main/scala/learn/spark/App.scala
+++ src/main/scala/learn/spark/App.scala
@@ -1,5 +1,6 @@
 package learn.spark
 
+import org.apache.spark.sql.functions.{count, countDistinct}
 import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
 import org.apache.spark.sql.{Row, SparkSession}
 import org.slf4j.{Logger, LoggerFactory}
@@ -8,26 +9,40 @@ object App {
   private val log: Logger = LoggerFactory.getLogger(App.getClass)
 
   def main(args: Array[String]): Unit = {
+    val userHomeDirectory = System.getProperty ("user.home")
+
     val spark = SparkSession
       .builder
       .master("local[*]")
       .appName("Simple Application")
       .getOrCreate()
 
-    spark.catalog.listTables().show()
-    val peopleRdd = spark.sparkContext.parallelize(
-      Array(Row(1L, "John", 30L), Row(2L, "Mary", 25L)
-    ))
-    val schema = new StructType(Array(StructField("id", LongType, nullable = false),
-      StructField("name", StringType, nullable = false),
-      StructField("age", LongType, nullable = true)))
+  // http://www.kaggle.com/usdot/flight-delays/data
+    val flights = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(userHomeDirectory + "/data/flights/flights.csv")
+    flights.show(5)
+    flights.select(countDistinct("origin_airport"),
+      countDistinct("destination_airport").as("dest_count"),
+      count("*").as("total_count"))
+      .show()
 
-    val peopleDf = spark.createDataFrame(peopleRdd, schema)
-    peopleDf.show()
-    peopleDf.select("name").show()
-    peopleDf.createOrReplaceTempView("people")
-    spark.catalog.listTables().show()
-    log.info("================== end.")
+
+    //    spark.catalog.listTables().show()
+    //    val peopleRdd = spark.sparkContext.parallelize(
+    //      Array(Row(1L, "John", 30L), Row(2L, "Mary", 25L)
+    //    ))
+    //    val schema = new StructType(Array(StructField("id", LongType, nullable = false),
+    //      StructField("name", StringType, nullable = false),
+    //      StructField("age", LongType, nullable = true)))
+    //
+    //    val peopleDf = spark.createDataFrame(peopleRdd, schema)
+    //    peopleDf.show()
+    //    peopleDf.select("name").show()
+    //    peopleDf.createOrReplaceTempView("people")
+    //    spark.catalog.listTables().show()
+    //    log.info("================== end.")
 
     //    val rdd = spark.sparkContext.parallelize(1 to 10)
     //      .map(x => (x, Random.nextInt(100) * x))
